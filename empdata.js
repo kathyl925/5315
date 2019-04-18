4c88c982890c70941e754cc55e874bdee5f5363a
kathy <liuqiuye@seattleu.edu>
2019-04-18 10:52:32 -0700

    empjson.html

--- empdata.js
+++ empdata.js
@@ -1,7 +1,7 @@
 {
   "data": [
     {
-      "name": "James Bond",
+      "name": "James Smith",
       "designation": "Accountant",
       "salary": "$170,750",
       "joining_date": "2011/07/25",
